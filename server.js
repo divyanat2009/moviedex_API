cb1f815f7ba080e21d689b9f2c8a987683a1e39a
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-06 10:06:15 -0600

    corrected error

--- server.js
+++ server.js
@@ -1,51 +1,47 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const MOVIEDEX = require("./movies-data-small.json");
+const app = express();
 
-const app = express()
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "DENIED!" });
   }
-  next()
-})
+  next();
+});
 
-app.get('/movie', function handleGetMovie(req, res) {
-  let response = MOVIES;
+app.get("/movie", function handleGenreTypes(req, res) {
+  const { genre, country, avg_vote } = req.query;
+  let response = MOVIEDEX;
 
-  if (req.query.genre) {
+  if (genre) {
     response = response.filter(movie =>
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
   }
-
-  if (req.query.country) {
+  if (country) {
     response = response.filter(movie =>
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
   }
-
-  if (req.query.avg_vote) {
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
-    )
+  if (avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(avg_vote)
+    );
   }
+  res.json(response);
+});
+const PORT = process.env.PORT || 8000;
 
-  res.json(response)
-})
-
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.listen(PORT);
