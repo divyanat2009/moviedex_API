85154a09d62c49f2fc17961a940ead6ac12be583
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-07 09:37:51 -0600

    aaded Prodfile and made corrections for production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require("./movies-data-small.json");
 const app = express();
 
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -41,6 +42,16 @@ app.get("/movie", function handleGenreTypes(req, res) {
   }
   res.json(response);
 });
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
